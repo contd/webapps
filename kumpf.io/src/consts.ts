440b05a5a2c8c46713b3f554c32353186e986985
Jason P Kumpf <kumpfjn@protonmail.ch>
2024-09-22 17:28:52 +0000

    Added new social media contstants

--- kumpf.io/src/consts.ts
+++ kumpf.io/src/consts.ts
@@ -3,3 +3,6 @@
 
 export const SITE_TITLE = 'Astro Blog';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SOC_LINK_MASTADON = 'https://m.webtoo.ls/@jasonkumpf';
+export const SOC_LINK_TWITTER = 'https://twitter.com/jasonpkumpf';
+export const SOC_LINK_GITHUB = 'https://github.com/contd';
